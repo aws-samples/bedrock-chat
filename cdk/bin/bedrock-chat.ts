750617b171ba832ed057ba876175adb17eeaa53b
statefb <tmgf.15.su@gmail.com>
2024-10-02 15:03:18 +0900

    remove ECS, cron schedule, vpc

--- cdk/bin/bedrock-chat.ts
+++ cdk/bin/bedrock-chat.ts
@@ -5,7 +5,6 @@ import { BedrockChatStack } from "../lib/bedrock-chat-stack";
 import { BedrockRegionResourcesStack } from "../lib/bedrock-region-resources";
 import { FrontendWafStack } from "../lib/frontend-waf-stack";
 import { TIdentityProvider } from "../lib/utils/identity-provider";
-import { CronScheduleProps } from "../lib/utils/cron-schedule";
 
 const app = new cdk.App();
 
@@ -35,20 +34,11 @@ const USER_POOL_DOMAIN_PREFIX: string = app.node.tryGetContext(
 const AUTO_JOIN_USER_GROUPS: string[] =
   app.node.tryGetContext("autoJoinUserGroups");
 
-const RDS_SCHEDULES: CronScheduleProps = app.node.tryGetContext("rdbSchedules");
 const ENABLE_MISTRAL: boolean = app.node.tryGetContext("enableMistral");
 const SELF_SIGN_UP_ENABLED: boolean =
   app.node.tryGetContext("selfSignUpEnabled");
 const ENABLE_KB: boolean = app.node.tryGetContext(
   "useBedrockKnowledgeBaseForRag"
-);
-
-// container size of embedding ecs tasks
-const EMBEDDING_CONTAINER_VCPU: number = app.node.tryGetContext(
-  "embeddingContainerVcpu"
-);
-const EMBEDDING_CONTAINER_MEMORY: number = app.node.tryGetContext(
-  "embeddingContainerMemory"
 );
 
 // how many nat gateways
@@ -102,8 +92,6 @@ const chat = new BedrockChatStack(app, `BedrockChatStack`, {
   rdsSchedules: RDS_SCHEDULES,
   enableMistral: ENABLE_MISTRAL,
   enableKB: ENABLE_KB,
-  embeddingContainerVcpu: EMBEDDING_CONTAINER_VCPU,
-  embeddingContainerMemory: EMBEDDING_CONTAINER_MEMORY,
   selfSignUpEnabled: SELF_SIGN_UP_ENABLED,
   natgatewayCount: NATGATEWAY_COUNT,
   documentBucket: bedrockRegionResources.documentBucket,
