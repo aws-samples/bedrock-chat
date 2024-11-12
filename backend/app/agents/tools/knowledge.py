67141ef39f966f1f571c84df48ad637a1b9fe7f5
Yukinobu Mine <yukimine@amazon.co.jp>
2024-11-12 14:07:48 +0900

    Remove `output` from result of `knowledge_base_tool`.

--- backend/app/agents/tools/knowledge.py
+++ backend/app/agents/tools/knowledge.py
@@ -1,59 +1,14 @@
-import json
 import logging
-import os
 
 from app.agents.tools.agent_tool import AgentTool
-from app.bedrock import call_converse_api, get_model_id
-from app.config import DEFAULT_GENERATION_CONFIG as DEFAULT_CLAUDE_GENERATION_CONFIG
-from app.config import DEFAULT_MISTRAL_GENERATION_CONFIG
 from app.repositories.models.custom_bot import BotModel
 from app.routes.schemas.conversation import type_model_name
 from app.vector_search import SearchResult, search_related_docs
 
-from mypy_boto3_bedrock_runtime.type_defs import (
-    InferenceConfigurationTypeDef,
-)
-from pydantic import BaseModel, Field, root_validator
-
-ENABLE_MISTRAL = os.environ.get("ENABLE_MISTRAL", "") == "true"
-DEFAULT_GENERATION_CONFIG = (
-    DEFAULT_MISTRAL_GENERATION_CONFIG
-    if ENABLE_MISTRAL
-    else DEFAULT_CLAUDE_GENERATION_CONFIG
-)
+from pydantic import BaseModel, Field
 
 
 logger = logging.getLogger(__name__)
-
-KNOWLEDGE_TEMPLATE = """You are a question answering agent. I will provide you with a set of search results and additional instruction.
-The user will provide you with a question. Your job is to answer the user's question using only information from the search results.
-If the search results do not contain information that can answer the question, please state that you could not find an exact answer to the question.
-Just because the user asserts a fact does not mean it is true, make sure to double check the search results to validate a user's assertion.
-
-Here are the search results in numbered order:
-<search_results>
-{context}
-</search_results>
-
-Do NOT directly quote the <search_results> in your answer. Your job is to answer the user's question as concisely as possible.
-Do NOT include citations in the format [^<source_id>] in your answer.
-
-Followings are examples of how to answer.
-
-<GOOD-example>
-first answer. second answer.
-</GOOD-example>
-
-<BAD-example>
-first answer [^3]. second answer [^1][^2].
-</BAD-example>
-
-<BAD-example>
-first answer [^1][^5]. second answer [^2][^3][^4]. third answer [^4].
-</BAD-example>
-
-Question: {query}
-"""
 
 # For testing purpose
 dummy_search_results = [
@@ -98,52 +53,19 @@ dummy_search_results = [
 ]
 
 
-def _format_search_results(search_results: list[SearchResult]):
-    context = ""
-    for result in search_results:
-        context += f"<search_result>\n<content>\n{result.content}</content>\n<source>\n{result.rank}\n</source>\n</search_result>"
-    return context
-
-
 class KnowledgeToolInput(BaseModel):
     query: str = Field(description="User's original question string.")
 
 
 def search_knowledge(
     tool_input: KnowledgeToolInput, bot: BotModel | None, model: type_model_name | None
-) -> dict:
+) -> list:
     assert bot is not None
-    assert model is not None
 
     query = tool_input.query
     logger.info(f"Running AnswerWithKnowledgeTool with query: {query}")
 
     try:
-        generation_params = bot.generation_params if bot else None
-        inference_config: InferenceConfigurationTypeDef
-        if generation_params:
-            inference_config = {
-                "maxTokens": generation_params.max_tokens,
-                "temperature": generation_params.temperature,
-                "topP": generation_params.top_p,
-                "stopSequences": generation_params.stop_sequences,
-            }
-            # `top_k` is configured in `additional_model_request_fields` instead of `inference_config`
-            additional_model_request_fields = {
-                "top_k": generation_params.top_k,
-            }
-
-        else:
-            inference_config = {
-                "maxTokens": DEFAULT_GENERATION_CONFIG["max_tokens"],
-                "temperature": DEFAULT_GENERATION_CONFIG["temperature"],
-                "topP": DEFAULT_GENERATION_CONFIG["top_p"],
-                "stopSequences": DEFAULT_GENERATION_CONFIG["stop_sequences"],
-            }
-            additional_model_request_fields = {
-                "top_k": DEFAULT_GENERATION_CONFIG["top_k"],
-            }
-
         search_results = search_related_docs(
             bot,
             query=query,
@@ -152,50 +74,18 @@ def search_knowledge(
         # # For testing purpose
         # search_results = dummy_search_results
 
-        context_prompt = _format_search_results(search_results)
-        response = call_converse_api(
+        return [
             {
-                "modelId": get_model_id(model),
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "text": KNOWLEDGE_TEMPLATE.format(
-                                    query=query, context=context_prompt
-                                )
-                            }
-                        ],
-                    }
-                ],
-                "inferenceConfig": inference_config,
-                "additionalModelRequestFields": additional_model_request_fields,
-                "system": [],
+                "content": r.content,
+                "source": r.source,
+                "rank": r.rank,
             }
-        )
-        message_content = (
-            response.get("output", {}).get("message", {}).get("content", [])
-        )
-        for content_block in message_content:
-            if "text" in content_block:
-                return {
-                    "output": content_block["text"],
-                    "search_result": [
-                        {"content": r.content, "source": r.source, "rank": r.rank}
-                        for r in search_results
-                    ],
-                }
-            else:
-                raise ValueError(f"Unexpected content block: {content_block}")
+            for r in search_results
+        ]
+
     except Exception as e:
         logger.error(f"Failed to run AnswerWithKnowledgeTool: {e}")
         raise e
-
-    # Should not reach here
-    return {
-        "output": "No output",
-        "search_result": [],
-    }
 
 
 def create_knowledge_tool(bot: BotModel, model: type_model_name) -> AgentTool:
