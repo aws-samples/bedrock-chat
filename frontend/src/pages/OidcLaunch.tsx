af8b47defdfabded6cf96496d8159884cc9249e7
Rohit S <rohit@qikrai.com>
2025-02-19 00:48:39 -0800

    Add type checking for states and reorganize the code for readability. Directly proceeding to next step leads to page hanging. This logic works but I still see a flash of screen that I don't expect to see.  I suspect this is a side effect of navigate not working. I am using window.location.reload as a workaround but that maybe causing other sideeffects.

--- frontend/src/pages/OidcLaunch.tsx
+++ frontend/src/pages/OidcLaunch.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate} from 'react-router-dom';
 import { getCurrentUser} from 'aws-amplify/auth';
-
 import {signOut, signIn, confirmSignIn} from 'aws-amplify/auth';
 
 
@@ -9,143 +8,147 @@ const OidcLaunch: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  // ltiSession tracks information for the current LTI session.
+  // We will likely need to move ltiSession further up in the component hierarchy,
+  // so other child components can access the state. Alternatively, we can store
+  // the session state in local storage and other components can read it from there.
   const [ltiSession, setLtiSession] = React.useState<any>(null);
-  // We will likely need to move ltiSession to the App so other child component can access the state. 
-  //   alternatively we can store the session state in local strage and other componetnes can read it from there.
 
-  const [username, setUsername] = useState('');
-  const [pageState, setPageState] = useState('loading');
-  // State moves through the following states in sequence except for the error state that can be reached at any time
-  // loading    : parse query params and get user sesion from local storage
-  // check_id   : state fetched. Prompt user if the user session is going to switch 
-  // start_auth : initiate auth with Cognito 
-  // done       : probably won't use this state. Instead we will directly navigate to main page. 
-  // error      : in case something goes wrong 
-  // 
-  // On succesful authetnication, we will navigate to the main page. Succesful authentication can happen under 2 conditions : 
-  //   1. in 'check_id' state when we discover that we are already logged in 
-  //   2. succesful authenticaiton during 'confirm' state
+  // username is the email id for the user currently logged in
+  const [username, setUsername] = useState<string>('');
+
+
+  enum PageState {
+    LOADING = 'LOADING',
+    CONFIRM = 'CONFIRM',
+    LOGGING_IN = 'LOGGING_IN',
+    REDIRECTING = 'REDIRECTING',
+    ERROR = 'ERROR'
+  };
+  const [pageState, setPageState] = useState<PageState>(PageState.LOADING);
+  // pageState representes a state machine for LTI based authentication. 
+  // LOADING : User will not see this screen in normal situations. 
+  //           Initial state while we fetch user details from local storage. We transition to 
+  //           other states based on the following tests. 
+  //           If current session different from LTI session, go to CONFIRM state  
+  //           If no current session, got to LOGGING_IN state
+  //           If current session is same as LTI session, go to REDIRECTING state  
+  // CONFIRM : Confirm with user if it is ok to switch login to the new user. This is only presented if
+  //          we are currently logged in as a different user. After user confiration, state will transition to
+  //          LOGGING_IN
+  // LOGGING_IN : Initiate auth with Cognito. If necessary, log out the current user.
+  // REDIRECTING : This state is presented briefly as we redirect. 
+  // ERROR : In case something goes wrong in any of the operations. This can only be reached 
   //
+  // On succesful authetnication, we will navigate to the main page. 
+
+
 
   // Run this effect only once when the component is mounted
-  
   useEffect(() => {
+    const setInitialPageState = async (_username: string) => {
+        setUsername(_username);
+
+        // LTI session info is passed in as query params. Extract and save state. 
+        const queryParams = new URLSearchParams(location.search);
+        const queryParamsObject: { [key: string]: string } = {};
+        queryParams.forEach((value, key) => {
+          queryParamsObject[key] = value;
+        });
+        //console.log('queryParamsObject', queryParamsObject);
+        setLtiSession(queryParamsObject);
+        const _email = queryParamsObject?.email;
+
+        if (_email === null) {
+          setPageState(PageState.ERROR);
+        } else if (_username === '') {
+          setPageState(PageState.LOGGING_IN);
+        } else if (_email !== _username) {
+          setPageState(PageState.CONFIRM);
+        } else {
+          setPageState(PageState.REDIRECTING);
+        }
+    }
     // check if we have an active user session
     getCurrentUser()
-        .then(user => {
-            console.log("User = ", user);
-            setUsername(user?.signInDetails?.loginId || '');
-        })
-        .catch(() => {
-            console.log("No session active");
-        })
+      .then(user => {
+        const _username = user?.signInDetails?.loginId || '';
+        setInitialPageState(_username);
+      })
+      .catch(() => {
+        setInitialPageState('');
+      });
 
-    // Parse query params that include LTI params from Canvas 
-    const queryParams = new URLSearchParams(location.search);
-    const queryParamsObject: { [key: string]: string } = {};
-    queryParams.forEach((value, key) => {
-      queryParamsObject[key] = value;
-    });
-    console.log('queryParamsObject', queryParamsObject);
-    setLtiSession(queryParamsObject);
-    if (ltiSession?.email === null) {
-      setPageState('error');
-    } else {
-      setPageState('check_id');
-    }
   }, [location]);
 
 
   const goHome = () => {
-    console.log('go home final ?? navigate and reload ');
-    //window.location.href = "/"
+    console.log('Navigating to home page');
     navigate('/');
     window.location.reload();
   }
 
-  const startSignIn = async (userSignedIn: boolean) => {
+  const startSignIn = async (logoutUser: boolean) => {
     try {
-      console.log('current user = ', username);
-
-      if (userSignedIn === true) {
+      if (logoutUser === true) {
         await signOut();
       }
       console.log(`login with *${ltiSession.email}*`);
 
-      const {isSignedIn, nextStep} = await signIn({
+      const {nextStep} = await signIn({
         username: ltiSession.email,
         options: {
           authFlowType: 'CUSTOM_WITHOUT_SRP'
         }
       });
-      console.log('signIn response :', isSignedIn, nextStep);
-      console.log('... ', nextStep?.signInStep);
       if (nextStep?.signInStep === 'CONFIRM_SIGN_IN_WITH_CUSTOM_CHALLENGE') {
-        console.log('Got challenge ', nextStep?.additionalInfo);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const user = await confirmSignIn({challengeResponse: 'opensesame'});
-        console.log('Confirm response', user);
+        //console.log('Confirm response', user);
         goHome();
       } else {
-        setPageState('error');
+        setPageState(PageState.ERROR);
 
       }
     } catch (err) {
       console.log('error = ', JSON.stringify(err));
-      alert('Incorrect username or password');
-      setPageState('error');
+      setPageState(PageState.ERROR);
     }
   }
 
-  if (pageState === 'loading') {
-    console.log('Loading state');
-    return <div>Loading state. This screen should only be visible for 1-2 seconds.  </div>;
+  //
+  // Render logic for the component 
+  // 
+
+  // set message based on pageState 
+  const NextStep = {
+    [PageState.LOADING]: { message: "Loading state. This screen should only be visible for 1-2 seconds.", action: null },
+    [PageState.ERROR]: { message: "Something went wrong !!", action: null },
+    [PageState.CONFIRM]: { message: `You are already logged in Qikr chat with ${username}. This request will switch your session to user ${ltiSession?.email}. Do you want to proceed ?`, action: () => startSignIn(true) },
+    [PageState.LOGGING_IN]: { message: `Logging in as ${ltiSession?.email}` , action: () => startSignIn(false) },
+    [PageState.REDIRECTING]: { message: "Logged in. Redirecting to the main app.", action: goHome }
+  };
+  const nextStep = NextStep[pageState];
+
+  /*
+  if (pageState === PageState.REDIRECTING) {
+    console.log('Redirecting to home page');
+    goHome();
   }
-
-  if (pageState === 'error') {
-    console.log('error -- lti session', ltiSession);
-    return <div>Something went wrong !! </div>;
+  if (pageState === PageState.LOGGING_IN) {
+    console.log('Proceeding to log in directly');
+    startSignIn(false);
   }
+  */
 
-  if (pageState === 'check_id') {
-    console.log('lti session = ', ltiSession);   // TODO remove debug print
-
-    // Already logged in. Launch the app
-    if (ltiSession.email === username) {
-      console.log(`User already logged in as ${ltiSession.email}. Forward to the main app`);
-      return (<div> 
-        <div> You are already logged in as {ltiSession.email}. We will reuse the current sesion. </div> 
-        <button onClick={() => goHome()} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
-          Continue
-        </button>
-      </div>)
-    }
-
-    // Not logged in. Start auth
-    if (username === '') {
-      console.log('No session. We will now create a new session.');
-      //startSignIn();
-      return (<div> 
-        <div> No session. We will now create a new session. </div> 
-        <button onClick={() => startSignIn(false)} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
-          Continue
-        </button>
-      </div>)
-      return <div>Logging in as {ltiSession.email}</div>;
-    }
-
-    // Logged in with different user. Check with user before logging in.
-    return (<div> 
-      <div> You are already logged in Qikr chat with {username}. </div> 
-      <div> This request will switch your session to user {ltiSession.email}. </div>
-      <div> Do you want to proceed ? </div>
-      <button onClick={() => startSignIn(true)} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
-        Yes
+  return (<div> 
+    <div> {nextStep.message} </div> 
+    {nextStep.action  && (
+      <button onClick={nextStep.action} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
+        Continue
       </button>
-    </div>)
-  }
-  return <div> Should not happen </div>;
-
-
+    )}
+  </div>);
 
 };
 
