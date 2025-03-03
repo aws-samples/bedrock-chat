7e9be0e8f73875b6d93962c8accb77773b5bc85d
tchangqikr <timothy@qikrai.com>
2025-03-03 14:26:43 -0800

    Allow tools to be disabled, take 2

--- backend/app/usecases/bot.py
+++ backend/app/usecases/bot.py
@@ -375,9 +375,14 @@ def modify_owned_bot(
     )
    
     # Build or update the agent if needed
-    tool_dict = {}
+    tool_dict = {tool.name: tool for tools in bot.agent.tools}
 
     if modify_input.agent:
+        # Remove tools that are turned off
+        removed_tools = set(tool_dict.keys()) - set(modify_input.agent.tools)
+        for tool_name in removed_tools:
+            del tool_dict[tool_name]
+        
         # Create or update AgentToolModel entries
         for tool_name in modify_input.agent.tools:
             try:
